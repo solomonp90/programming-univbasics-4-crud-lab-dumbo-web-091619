40f4d80744f50caaf9fc17ab2a111227e2f11e7a
Solomon R Pena <solomonraulpena@gmail.com>
2019-08-27 18:40:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  []
+  [];
 end
 
 def create_an_array
-  ["ruby","javascript","css","html"]
+  ["ruby","javascript","css","html"];
 end
 
 def add_element_to_end_of_array(array, element)
-array<<element
+array<<element;
 end
 
 def add_element_to_start_of_array(array, element)
-array.unshift(element)
+array.unshift(element);
 end
 
 def remove_element_from_end_of_array(array)
